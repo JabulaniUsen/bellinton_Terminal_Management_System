b45a6319718156763e96274cc623a76aa0e46eb2
Usenobong Usen Etokakpan <jabulanietokakpan@gmail.com>
2024-02-24 10:48:23 +0100

    Minor change

--- tms/src/App.jsx
+++ tms/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import CreateAccount from './Pages/CreateAccount'; // Ensure correct path
+import CreateAccount from './Pages/CreateAccount';
 import Signin from './Pages/Signin';
 import PasswordResetInstructions from './Pages/AccountRecovery/PasswordResetInstructions';
 import ForgottenPassword from './Pages/AccountRecovery/ForgottenPassword';
@@ -11,12 +11,12 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Signin />} />
         <Route path="/create-account" element={<CreateAccount />} />
-        <Route path='/' element={<Signin/>} />
-        <Route path='/forgotten-password' element={<ForgottenPassword/>} />
-        <Route path='/reset-password-instructions' element={<PasswordResetInstructions/>} />
-        <Route path='/change-password' element={<ChangePassword/>} />
-        <Route path='/dashboard' element={<Dashboard/>} />
+        <Route path='/forgotten-password' element={<ForgottenPassword />} />
+        <Route path='/reset-password-instructions' element={<PasswordResetInstructions />} />
+        <Route path='/change-password' element={<ChangePassword />} />
+        <Route path='/dashboard' element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
