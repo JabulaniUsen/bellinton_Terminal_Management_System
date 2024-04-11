4e27a913499dfa457f0ffe2d4c88b3b2d8e8c194
Usenobong Usen Etokakpan <jabulanietokakpan@gmail.com>
2024-04-11 12:21:34 +0100

    Adding Queue

--- tms/src/Dashboard/Tabs/YardManagementComponent/ContainerMovementUpdate.jsx
+++ tms/src/Dashboard/Tabs/YardManagementComponent/ContainerMovementUpdate.jsx
@@ -59,17 +59,21 @@ const ContainerMovementUpdate = () => {
               <label htmlFor="description" className="block font-semibold text-base">Description: </label>
               <textarea className='border-gray-400 border-[1px] rounded-lg p-4 w-[300px]' required name="description" id="description" rows="4"></textarea>
             </div>
-            <div className="flex justify-between items-center my-5">
-              <label htmlFor="emptyGateOutDate" className="block font-semibold text-base">Empty Gate-Out Date:</label>
-              <input type='date' className='border-gray-400 border-[1px] rounded-lg p-2 w-[300px]' required id="emptyGateOutDate" name="emptyGateOutDate" />
+            <div className="flex justify-between items-center w-[60%] my-5">
+              <label htmlFor="emptyGateOutDate" className="block font-semibold text-base">Dsicharge Off-Out Date:</label>
+              <input type='date' className='border-gray-400 border-[1px] rounded-lg p-2 w-[400px]' required id="emptyGateOutDate" name="emptyGateOutDate" />
             </div>
-            <div className="flex justify-between items-center my-5">
-              <label htmlFor="exportGateInDate" className="block font-semibold text-base">Export Gate-In Date:</label>
-              <input type='date' className='border-gray-400 border-[1px] rounded-lg p-2 w-[300px]' required id="exportGateInDate" name="exportGateInDate" />
+            <div className="flex justify-between items-center w-[60%] my-5">
+              <label htmlFor="exportGateInDate" className="block font-semibold text-base">Port Gate-In Date:</label>
+              <input type='date' className='border-gray-400 border-[1px] rounded-lg p-2 w-[400px]' required id="exportGateInDate" name="exportGateInDate" />
             </div>
-            <div className="flex justify-between items-center my-5">
-              <label htmlFor="loadedOnboardDate" className="block font-semibold text-base">Loaded Onboard Date:</label>
-              <input type='date' className='border-gray-400 border-[1px] rounded-lg p-2 w-[300px]' required id="loadedOnboardDate" name="loadedOnboardDate" />
+            <div className="flex justify-between items-center w-[60%] my-5">
+              <label htmlFor="loadedOnboardDate" className="block font-semibold text-base">Port Gate-In Date:</label>
+              <input type='date' className='border-gray-400 border-[1px] rounded-lg p-2 w-[400px]' required id="loadedOnboardDate" name="loadedOnboardDate" />
+            </div>
+            <div className="flex justify-between items-center w-[60%] my-5">
+              <label htmlFor="loadedOnboardDate" className="block font-semibold text-base">Loaded On-board Date:</label>
+              <input type='date' className='border-gray-400 border-[1px] rounded-lg p-2 w-[400px]' required id="loadedOnboardDate" name="loadedOnboardDate" />
             </div>
             <div className="buttons flex gap-3 mx-[200px] mt-10">
               <button className='text-white bg-[#4000FF] rounded-md py-1 px-10' type='submit' onClick={handleUpdate}>Update</button>
--- tms/src/Dashboard/Tabs/YardManagementComponent/YardQueueManagement.jsx
+++ tms/src/Dashboard/Tabs/YardManagementComponent/YardQueueManagement.jsx
@@ -1,3 +1,5 @@
+import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useState } from 'react';
 import Select from 'react-select';
 import { toast, ToastContainer } from 'react-toastify';
@@ -5,6 +7,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const YardQueueManagement = () => {
   const [showReport, setShowReport] = useState(false)
+    const [containerCount, setContainerCount] = useState(3); 
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -14,12 +17,42 @@ const YardQueueManagement = () => {
   const handleShowReport = () => {
     setShowReport(true)
   }
+
   const options2 = [
     { value: 'Port of Lagos', label: 'Port of Lagos' },
     { value: 'Port of Chicago', label: 'Port of Chicago' },
     { value: 'Port of London', label: 'Port of London' },
     { value: 'Port of Dubai', label: 'Port of Dubai' },
   ];
+
+  const truck = [
+    { value: 'TRK231', label: 'TRK231' },
+    { value: 'TRK112', label: 'TRK112' },
+    { value: 'TRK983', label: 'TRK983' },
+    { value: 'TRK532', label: 'TRK532' },
+  ];
+
+  const driver = [
+    { value: 'John Doe', label: 'John Doe' },
+    { value: 'Doe Mike', label: 'Doe Mike' },
+    { value: 'Emeka Sunday', label: 'Emeka Sunday' },
+    { value: 'Femi Ola', label: 'Femi Ola' },
+  ];
+
+  const priority = [
+    { value: 'High', label: 'High' },
+    { value: 'Medium', label: 'Medium' },
+    { value: 'Low', label: 'Low' },
+  ];
+
+  const handleAddContainer = () => {
+    setContainerCount(containerCount + 1);
+  };
+
+  const handleDeleteContainer = (index) => {
+    setContainerCount(containerCount - 1);
+  };
+
   return (
     <div className="">
       {!showReport ? (
@@ -42,54 +75,103 @@ const YardQueueManagement = () => {
                     required
                 />
               </div>
+              <div className="flex justify-between items-center w-[70%]">
+                  <label htmlFor="description" className="block font-semibold text-base">Assigned Truck: </label>
+                  <Select
+                    options={truck}
+                    isSearchable
+                    className='w-[400px]'
+                    required
+                />
+              </div>
+              <div className="flex justify-between items-center w-[70%] my-2">
+                  <label htmlFor="emptyGateOutDate" className="block font-semibold text-base">Assigned Driver:</label>
+                  <Select
+                    options={driver}
+                    isSearchable
+                    className='w-[400px]'
+                    required
+                />
+              </div>
+              <div className="flex justify-between items-center w-[70%] my-2">
+                  <label htmlFor="emptyGateOutDate" className="block font-semibold text-base">Priority:</label>
+                  <Select
+                    options={priority}
+                    isSearchable
+                    className='w-[400px]'
+                    placeholder="High, Medium, Low"
+                    required
+                />
+              </div>
             </div>
   
-            <div className="flex justify-between items-center w-[70%] my-7">
-              <label htmlFor="location" className="text-lg font-bold">Gate-in Date: </label>
-              <input type='date' className='border-gray-400 border-[1px] rounded-lg p-2 w-[400px]' required/>
+  
+            <div className="my-10">
+              <h2 className='text-lg font-bold'>Merge Containers:</h2>
+              <ul className='list-disc ml-6 my-3'>
+                <li className='text-lg font-bold'>Select Containers to Merge:</li>
+              </ul>
+              {Array.from({ length: containerCount }, (_, index) => (
+                <div className="">
+                    <div key={index} className="flex justify-between items-center w-[70%] my-2">
+                        <label htmlFor={`container${index + 1}`} className="block font-semibold text-base">{`Container ${index + 1}:`}</label>
+                        <input type='text' id={`container${index + 1}`} className='border-gray-400 border-[1px] rounded-lg p-2 w-[400px]' required />
+                        <button type="button" className='text-red-700' onClick={() => handleDeleteContainer(index)}><FontAwesomeIcon icon={faTrashCan}/></button>
+                    </div>
+                </div>
+              ))}
+              <button className='text-blue-600 underline' type="button" onClick={handleAddContainer}>(Add more containers as needed here)</button>
             </div>
   
             <div className="my-10">
-              <h2 className='text-lg font-bold'>Truck Information:</h2>
+              <h2 className='text-lg font-bold'>Merge Destination:</h2>
+              <ul className='list-disc ml-6 my-3'>
+                <li className='text-lg font-bold'>Select Containers to Merge:</li>
+              </ul>
               <div className="flex justify-between w-[70%] mt-7">
-                  <label htmlFor="description" className="block font-semibold text-base">License Plate Number: </label>
-                  <input type='text' className='border-gray-400 border-[1px] rounded-lg p-2 w-[400px]' required/>
+                  <label htmlFor="description" className="block font-semibold text-base">Assigned Truck: </label>
+                  <Select
+                    options={truck}
+                    isSearchable
+                    className='w-[400px]'
+                    required
+                />
               </div>
               <div className="flex justify-between items-center w-[70%] my-2">
-                  <label htmlFor="emptyGateOutDate" className="block font-semibold text-base">Driver's License Number:</label>
-                  <input type='text' className='border-gray-400 border-[1px] rounded-lg p-2 w-[400px]' required />
-              </div>
-              <div className="flex justify-between items-center w-[70%] my-2">
-                  <label htmlFor="exportGateInDate" className="block font-semibold text-base">Driver's Name: </label>
-                  <input type='text' className='border-gray-400 border-[1px] rounded-lg p-2 w-[400px]' required/>
-              </div>
-              <div className="flex justify-between items-center w-[70%] my-2">
-                  <label htmlFor="loadedOnboardDate" className="block font-semibold text-base">Company/Organization:</label>
-                  <input type='text' className='border-gray-400 border-[1px] rounded-lg p-2 w-[400px]' required />
+                  <label htmlFor="emptyGateOutDate" className="block font-semibold text-base">Assigned Driver:</label>
+                  <Select
+                    options={driver}
+                    isSearchable
+                    className='w-[400px]'
+                    required
+                />
               </div>
             </div>
   
             <div className="my-10">
-              <h2 className='text-lg font-bold'>Security Check:</h2>
-              <div className="flex justify-between items-center w-[32%] mt-5">
-                  <label htmlFor="containerWidth" className="block font-semibold text-base">Hazardous Materials Check: </label>
-                  <input type='checkbox' className='w-[20px] h-[20px]' required/>
+              <h2 className='text-lg font-bold'>Container Assignment:</h2>
+              <div className="flex justify-between items-center w-[70%] mt-5">
+                  <label htmlFor="containerWidth" className="block font-semibold text-base">Total Containers in Queue: </label>
+                  <input type='text' readOnly className='border-gray-400 border-[1px] rounded-lg p-2 w-[400px]' required/>
               </div>
-              <div className="flex justify-between items-center w-[32%] my-4">
-                  <label htmlFor="movementType" className="block font-semibold text-base">Security Clearance Check: </label>
-                  <input type='checkbox' className='w-[20px] h-[20px]' required/>
+              <div className="flex justify-between items-center w-[70%] my-2">
+                  <label htmlFor="movementType" className="block font-semibold text-base">Average Queue Waiting Time: </label>
+                  <input type='text' readOnly className='border-gray-400 border-[1px] rounded-lg p-2 w-[400px]' required/>
               </div>
-              <div className="flex justify-between items-center w-[32%] my-4">
-                  <label htmlFor="movementType" className="block font-semibold text-base">Temperature-sensitive Cargo:  </label>
-                  <input type='checkbox' className='w-[20px] h-[20px]' required/>
+              <div className="flex justify-between items-center w-[70%]">
+                  <label htmlFor="description" className="block font-semibold text-base">Average Container Processing Time: </label>
+                  <input type='text' readOnly className='border-gray-400 border-[1px] rounded-lg p-2 w-[400px]' required/>
+              </div>
+              <div className="flex justify-between items-center w-[70%] my-2">
+                  <label htmlFor="emptyGateOutDate" className="block font-semibold text-base">Estimated Time of Next Container Assignment:</label>
+                  <input type='text' readOnly className='border-gray-400 border-[1px] rounded-lg p-2 w-[400px]' required/>
               </div>
             </div>
-  
   
             <div className="buttons flex w-[70%] gap-3 mx-[200px] mt-10">
-              <button className='text-white bg-[#4000FF] rounded-md py-1 px-10' type='submit'>Submit</button>
-              <button className='text-white bg-[#828282] rounded-md py-1 px-10' type='reset'>Reset</button>
-              <p className='text-white bg-[#4000FF] rounded-md py-1 px-10 cursor-pointer' onClick={handleShowReport}>View Entry Details</p>
+              <button className='text-white bg-[#4000FF] rounded-md py-1 px-10'>Assign Container</button>
+              <button className='text-white bg-[#4000FF] rounded-md py-1 px-10'>Merge Containers</button>
+              <button className='text-white bg-[#4000FF] rounded-md py-1 px-10'>View Queue</button>
             </div>
           </form>
         </div>
