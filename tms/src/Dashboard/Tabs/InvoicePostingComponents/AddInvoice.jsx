98a08780aeb082d4b48d5a4efe45d1f555b2b69b
Usenobong Usen Etokakpan <jabulanietokakpan@gmail.com>
2024-05-13 03:05:00 +0100

    Fixing bugs

--- tms/src/Dashboard/Tabs/InvoicePostingComponents/AddInvoice.jsx
+++ tms/src/Dashboard/Tabs/InvoicePostingComponents/AddInvoice.jsx
@@ -1,4 +1,4 @@
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { faCircleCheck, faMagnifyingGlass, faUpload } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useState, useEffect, useRef } from 'react';
 import { toast } from 'react-toastify';
@@ -9,8 +9,20 @@ import UploadBox from '../ManifestComponents/UploadBox';
 const AddInvoice = () => {
   const [inputValue, setInputValue] = useState('');
   const [suggestions, setSuggestions] = useState([]);
+  const [inputValue2, setInputValue2] = useState('');
+  const [suggestions2, setSuggestions2] = useState([]);
   const inputRef = useRef(null);
   const [showUpload, setShowUpload] = useState(false)
+  const [isFileUploaded, setIsFileUploaded] = useState(false);
+
+
+  const handleFileChange = (event) => {
+      if (event.target.files.length > 0) {
+        setIsFileUploaded(true);
+      } else {
+        setIsFileUploaded(false);
+      }
+    };
   
 
   useEffect(() => {
@@ -71,6 +83,7 @@ const AddInvoice = () => {
   };
 
   const data = ["1001", "1002", "1003", "1004", "1005"]
+  const customerNameData = ["Michael", "Kate", "Williams", "Jabulani"]
 
   const handleInputChange = (e) => {
     const value = e.target.value;
@@ -85,6 +98,21 @@ const AddInvoice = () => {
   const handleSuggestionClick = (suggestion) => {
     setInputValue(suggestion);
     setSuggestions([]);
+  };
+
+  const handleInputChange2 = (e) => {
+    const value = e.target.value;
+    setInputValue2(value);
+
+    // Filter suggestions based on the input value
+    const filteredSuggestions2 = customerNameData.filter((item) =>
+      item.toLowerCase().includes(value.toLowerCase())
+    );
+    setSuggestions2(filteredSuggestions2);
+  };
+  const handleSuggestionClick2 = (suggestion2) => {
+    setInputValue2(suggestion2);
+    setSuggestions2([]);
   };
 
   // Upload 
@@ -104,9 +132,10 @@ const AddInvoice = () => {
       </div>
 
       <form action="" onSubmit={handleSubmit} >
-        <div className='my-10 grid grid-cols-2'>
-          <div className="sideOne">
-            <div className="flex flex-col gap-2 my-5">
+        <div className=''>
+            <h3 className='mt-10 mb-2 font-bold'>Invoice Details</h3>
+            <div className="grid grid-cols-2">
+                <div className="flex flex-col gap-2 my-2">
                 <label htmlFor="name" className='text-base font-semibold'>Customer ID:</label>
                 <div ref={inputRef}>
                     <div className="flex items-center justify-between pr-3 pl-2 py-2 rounded-md border-gray-500 border w-[400px]">
@@ -114,81 +143,112 @@ const AddInvoice = () => {
                         type="text"
                         value={inputValue}
                         onChange={handleInputChange}
-                        className='outline-none'
-                    />
-                    <FontAwesomeIcon icon={faMagnifyingGlass} className='text-[#999999]' />
+                        className='outline-none w-full'
+                        />
+                        <FontAwesomeIcon icon={faMagnifyingGlass} className='text-[#999999]' />
+                    </div>
+                    <ul className=''>
+                        {suggestions.map((suggestion, index) => (
+                        <li key={index} className='cursor-pointer hover:bg-slate-100 p-2' onClick={() => handleSuggestionClick(suggestion)}>
+                        {suggestion}
+                        </li>
+                        ))}
+                    </ul>
                 </div>
-                <ul className=''>
-                    {suggestions.map((suggestion, index) => (
-                    <li key={index} className='cursor-pointer hover:bg-slate-100 p-2' onClick={() => handleSuggestionClick(suggestion)}>
-                    {suggestion}
-                    </li>
-                    ))}
-                </ul>
+                </div>
+                <div className="flex flex-col gap-2 my-2">
+                <label htmlFor="name" className='text-base font-semibold'>Customer Name:</label>
+                <div ref={inputRef}>
+                    <div className="flex items-center justify-between pr-3 pl-2 py-2 rounded-md border-gray-500 border w-[400px]">
+                    <input
+                        type="text"
+                        value={inputValue2}
+                        onChange={handleInputChange2}
+                        className='outline-none w-full'
+                        />
+                        <FontAwesomeIcon icon={faMagnifyingGlass} className='text-[#999999]' />
+                    </div>
+                    <ul className=''>
+                        {suggestions2.map((suggestions2, index) => (
+                        <li key={index} className='cursor-pointer hover:bg-slate-100 p-2' onClick={() => handleSuggestionClick2(suggestions2)}>
+                        {suggestions2}
+                        </li>
+                        ))}
+                    </ul>
+                </div>
+                </div>
+                <div className="flex flex-col gap-2 my-2">
+                <label htmlFor="name" className='text-base font-semibold'>Invoice Date:</label>
+                <input required type="date" className='rounded-lg p-2 border border-gray-500 outline-none w-[400px]' id="email" name="email"  />
+                </div>
+                <div className="flex flex-col gap-2 my-2">
+                <label htmlFor="name" className='text-base font-semibold'>Invoice Number:</label>
+                <input required type="number" className='rounded-lg p-2 border border-gray-500 outline-none w-[400px]' id="phoneNumber" name="phoneNumber" placeholder='Enter invoice number:' />
+                </div>
+                <div className="flex flex-col gap-2 my-2">
+                <label htmlFor="name" className='text-base font-semibold'>Total Amount Due:</label>
+                <input required type="text" className='rounded-lg p-2 border border-gray-500 outline-none w-[400px]' id="contactPerson" name="contactPerson" placeholder='Enter total amount due' />
+                </div>
+
+                <div className="flex flex-col gap-2 my-2">
+                <label htmlFor="name" className='text-base font-semibold'>Service Type:</label>
+                {/* <input required type="text" className='rounded-lg p-2 border border-gray-500 outline-none w-[400px]' id="address" name="address" placeholder='Enter address:' /> */}
+                    <select name="" id="" className='rounded-lg p-2 border border-gray-500 outline-none w-[400px]'>
+                        <option value="" className='text-[#0000002a]'>Select service type</option>
+                        <option value="type 1">type 1</option>
+                        <option value="type 2">type 2</option>
+                        <option value="type 3">type 3</option>
+                    </select>
                 </div>
             </div>
-            <div className="flex flex-col gap-2 my-5">
-              <label htmlFor="name" className='text-base font-semibold'>Customer Name:</label>
-              <div ref={inputRef}>
-                <div className="flex items-center justify-between pr-3 pl-2 py-2 rounded-md border-gray-500 border w-[400px]">
-                  <input
-                    type="text"
-                    value={inputValue}
-                    onChange={handleInputChange}
-                    className='outline-none'
-                />
-                <FontAwesomeIcon icon={faMagnifyingGlass} className='text-[#999999]' />
-              </div>
-              <ul className=''>
-                {suggestions.map((suggestion, index) => (
-                <li key={index} className='cursor-pointer hover:bg-slate-100 p-2' onClick={() => handleSuggestionClick(suggestion)}>
-                  {suggestion}
-                </li>
-                ))}
-              </ul>
-            </div>
-            <div className="flex flex-col gap-2 my-5">
-              <label htmlFor="name" className='text-base font-semibold'>Email:</label>
-              <input required type="Email" className='rounded-lg p-2 border border-gray-500 outline-none w-[400px]' id="email" name="email" placeholder='Enter email address:' />
-            </div>
-            <div className="flex flex-col gap-2 my-5">
-              <label htmlFor="name" className='text-base font-semibold'>Phone Number:</label>
-              <input required type="number" className='rounded-lg p-2 border border-gray-500 outline-none w-[400px]' id="phoneNumber" name="phoneNumber" placeholder='Enter phone number:' />
-            </div>
-            <div className="flex flex-col gap-2 my-5">
-              <label htmlFor="name" className='text-base font-semibold'>Contact Person:</label>
-              <input required type="text" className='rounded-lg p-2 border border-gray-500 outline-none w-[400px]' id="contactPerson" name="contactPerson" placeholder='Enter contact person' />
-            </div>
-          </div>
+            
+            <h3 className='mt-10 mb-2 font-bold'>Service Details</h3>
 
-          <div className="sideTwo">
-            <div className="flex flex-col gap-2 my-5">
-              <label htmlFor="name" className='text-base font-semibold'>Address:</label>
-              <input required type="text" className='rounded-lg p-2 border border-gray-500 outline-none w-[400px]' id="address" name="address" placeholder='Enter address:' />
-            </div>
-            <div className="flex flex-col gap-2 my-5">
-              <label htmlFor="name" className='text-base font-semibold'>City:</label>
-              <input required type="text" className='rounded-lg p-2 border border-gray-500 outline-none w-[400px]' id="city" name="city" placeholder='Enter city:' />
-            </div>
-            <div className="flex flex-col gap-2 my-5">
-              <label htmlFor="name" className='text-base font-semibold'>Country:</label>
-              <input required type="text" className='rounded-lg p-2 border border-gray-500 outline-none w-[400px]' id="country" name="country" placeholder='Enter country:' />
-            </div>
-            <div className="flex flex-col gap-2 my-5">
-              <label htmlFor="name" className='text-base font-semibold'>State/Province:</label>
-              <input required type="text" className='rounded-lg p-2 border border-gray-500 outline-none w-[400px]' id="State/Province" name="State/Province" placeholder='Enter State/Province:' />
-            </div>
-            <div className="flex flex-col gap-2 my-5">
-              <label htmlFor="name" className='text-base font-semibold'>Postal Code:</label>
-              <input required type="number" className='rounded-lg p-2 border border-gray-500 outline-none w-[400px]' id="postalCode" name="postalCode" placeholder='Enter Postal Code:' />
+            <div className="grid grid-cols-2">
+                <div className="flex flex-col gap-2 my-2">
+                <label htmlFor="name" className='text-base font-semibold'>Export Handling Fees:</label>
+                <input required type="text" className='rounded-lg p-2 border border-gray-500 outline-none w-[400px]' id="city" name="city" placeholder='Enter Export Handling Fees:' />
+                </div>
+                <div className="flex flex-col gap-2 my-2">
+                <label htmlFor="name" className='text-base font-semibold'>Storage Charge:</label>
+                <input required type="text" className='rounded-lg p-2 border border-gray-500 outline-none w-[400px]' id="Storage Charge" name="Storage Charge" placeholder='Enter Storage Charge:' />
+                </div>
+                <div className="flex flex-col gap-2 my-2">
+                <label htmlFor="name" className='text-base font-semibold'>Haulage (Truck):</label>
+                <input required type="text" className='rounded-lg p-2 border border-gray-500 outline-none w-[400px]' id="Haulage" name="Haulage" placeholder='Enter Haulage:' />
+                </div>
+
+
+                <div className="flex flex-col gap-2 my-2">
+                <label htmlFor="name" className='text-base font-semibold'>Discounts or Adjustments:</label>
+                <input required type="text" className='rounded-lg p-2 border border-gray-500 outline-none w-[400px]' id="Discounts or Adjustments" name="Discounts or Adjustments" placeholder='Enter Discounts or Adjustments:' />
+                </div>
+                <div className="flex flex-col gap-2 my-2">
+                <label htmlFor="name" className='text-base font-semibold'>Rated upto Date:</label>
+                <input required type="date" className='rounded-lg p-2 border border-gray-500 outline-none w-[400px]' id="Rated upto Date" name="Rated upto Date"/>
+                </div>
             </div>
 
-          </div>
         </div>
-        <div className="flex justify-center items-center gap-5 text-lg">
-          <p onClick={handleUpload} className='underline text-[#4e9352] font-semibold cursor-pointer'>Upload CSV/XLS</p>
-          <button type="submit" className='bg-[#4e9352] hover:bg-[#4e93518c] rounded-lg text-white px-10 py-2'>Add Customer</button>
-          <button type="reset" className='bg-[#828282] rounded-lg text-white px-12 py-2'>Reset</button>
+        <div className="flex justify-center items-center gap-5 text-lg my-10">
+          <p onClick={handleUpload} className='underline text-[#4e9352] font-semibold cursor-pointer'>Upload Customer Invoice</p>
+          <div className="">
+                <small>
+                    <input type="file" name="" id="attachment" className='hidden' onChange={handleFileChange} />
+                    {!isFileUploaded ? (
+                         <label htmlFor="attachment" className='text-blue-500 cursor-pointer'>
+                            <FontAwesomeIcon icon={faUpload} className='mr12'/>
+                            Attachments(e.g., photos, documents)
+                        </label>
+                    ) : (
+                        <label className='text-green-600'>
+                            <FontAwesomeIcon icon={faCircleCheck} className='mr-1' />
+                            File uploaded successfully!
+                        </label>
+                    )}
+                </small>
+            </div>
+          <button type="submit" className='bg-[#4e9352] hover:bg-[#4e93518c] rounded-lg text-white px-10 py-2'>Post Export Invoice</button>
         </div>
       </form>
       { showUpload &&
